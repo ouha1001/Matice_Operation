3b122dab5732a6b1838152da824a19895ff0a507
Oussama Hamioui <HamiouiO@corning.com>
2024-08-27 16:26:13 +0200

    '######

---
+++ Matrice_Operation/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using PayPal;
+using PayPal.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Matrice_Operation
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+    }
+
+}
