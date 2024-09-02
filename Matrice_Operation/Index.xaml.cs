e001d803ccc3f95466be70596a8586d98ddf5dec
Oussama Hamioui <HamiouiO@corning.com>
2024-09-02 16:21:18 +0200

    001

--- Matrice_Operation/Index.xaml.cs
+++ Matrice_Operation/Index.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Matice_Operation
+
+namespace Matrice_Operation
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
@@ -590,7 +579,7 @@ namespace Matice_Operation
 
             M1.Content = "";
 
-
+            
             int val = Convert.ToInt32(size.Text);
             for (int i = 0; i < val; i++)
             {
